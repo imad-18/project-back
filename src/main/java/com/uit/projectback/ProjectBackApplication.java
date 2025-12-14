136088fcb3e4081d90862fff97406f784d2fd5b6
imad-18 <imad.labrini@uit.ac.ma>
2025-12-14 16:39:45 +0100

    update properties and pom.xml files

--- src/main/java/com/uit/projectback/ProjectBackApplication.java
+++ src/main/java/com/uit/projectback/ProjectBackApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectBackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectBackApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBackApplication.class, args);
+    }
 
 }
